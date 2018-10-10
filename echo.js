2a838ebf95b587aa521ca92a343858aae13c57d4
GauriTikekar <gauri.tikekar@summitaustralia.com.au>
2018-10-10 17:02:39 +1100

    Echo input for certain number of times

---
+++ echo.js
@@ -0,0 +1,9 @@
+console.log("hello");
+echo("hello", 5)
+
+//function to print the 'input' for certain number of times
+function echo (input, count) {
+	for(var i=0; i< count; i++){
+		console.log(input);
+	}
+}
